d11afc5ca0450a0d65954b402e8d7f2c80e555db
FlamestarRS <FlamestarRS@gmail.com>
2026-01-29 14:29:51 -0500

    read lines from channel

--- main.go
+++ main.go
@@ -13,26 +13,39 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	currentLine := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err == io.EOF {
-			break
-		}
+	linesChan := getLinesChannel(f)
 
-		split := strings.Split(string(data[:n]), "\n")
-
-		for i := 0; i < len(split)-1; i++ {
-			currentLine += split[i]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
-		currentLine += split[len(split)-1]
+	for i := range linesChan {
+		fmt.Printf("read: %s\n", i)
 	}
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
-	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channel := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(channel)
+		currentLine := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err == io.EOF {
+				break
+			}
+
+			split := strings.Split(string(data[:n]), "\n")
+
+			for i := 0; i < len(split)-1; i++ {
+				currentLine += split[i]
+				channel <- currentLine
+				currentLine = ""
+			}
+			currentLine += split[len(split)-1]
+		}
+		if currentLine != "" {
+			channel <- currentLine
+		}
+	}()
+	return channel
 }
