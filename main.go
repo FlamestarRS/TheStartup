8c27af423f8efce5c634eb2da6b82c9f43e4cd23
FlamestarRS <FlamestarRS@gmail.com>
2026-01-29 14:11:29 -0500

    read files 8 bytes at a time, printing each line

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,12 +15,24 @@ func main() {
 	}
 	defer f.Close()
 
+	currentLine := ""
 	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
 		if err == io.EOF {
 			break
 		}
-		fmt.Printf("read: %s\n", data[:n])
+
+		split := strings.Split(string(data[:n]), "\n")
+
+		for i := 0; i < len(split)-1; i++ {
+			currentLine += split[i]
+			fmt.Printf("read: %s\n", currentLine)
+			currentLine = ""
+		}
+		currentLine += split[len(split)-1]
+	}
+	if currentLine != "" {
+		fmt.Printf("read: %s\n", currentLine)
 	}
 }
