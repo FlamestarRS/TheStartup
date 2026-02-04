ac0380abf8f890d0425e8c72e10973c7c716a0ac
FlamestarRS <FlamestarRS@gmail.com>
2026-02-04 15:42:34 -0500

    check headers for valid chars

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -20,15 +21,15 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	if index == 0 {
 		return 2, true, nil
 	}
-	header := bytes.SplitN(data[:index], []byte(":"), 2)
-	key := string(header[0])
-	if strings.HasSuffix(key, " ") {
-		return 0, false, fmt.Errorf("Error: field-name has trailing whitespace")
-	}
-	value := string(header[1])
 	numBytes := index + 2
-	key = strings.TrimSpace(key)
-	value = strings.TrimSpace(strings.Trim(value, "\r\n"))
+	header := bytes.SplitN(data[:index], []byte(":"), 2)
+	key := strings.TrimLeft(string(header[0]), " ")
+	validChars := regexp.MustCompile("^[a-zA-Z0-9!#$%&'*+-.^_`|~]+$")
+	if !validChars.MatchString(key) {
+		return 0, false, fmt.Errorf("Error: field-name contains invalid characters: %s", key)
+	}
+	key = strings.ToLower(key)
+	value := strings.TrimSpace(string(header[1]))
 
 	h[key] = value
 	return numBytes, false, nil
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeaders(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -51,7 +51,7 @@ func TestHeaders(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, headers)
 	assert.Equal(t, "localhost:42069", headers["host"])
-	assert.Equal(t, "curl/7.81.0", headers["User-Agent"])
+	assert.Equal(t, "curl/7.81.0", headers["user-agent"])
 	assert.Equal(t, 25, n)
 	assert.False(t, done)
 
@@ -79,7 +79,15 @@ func TestHeaders(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 57, n)
+	assert.False(t, done)
+
+	// Test: Invalid characters in header
+	headers = NewHeaders()
+	data = []byte("       @@@@@Host: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
 	assert.False(t, done)
 }
